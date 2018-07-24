21f9882101ccf47b27634f1e3d5f51e90cf81945
lukasheinrich <lukas.heinrich@gmail.com>
2018-07-24 17:05:38 +0200

    go

--- docs/launch_recast.sh
+++ docs/launch_recast.sh
@@ -1,5 +1,12 @@
-function launch_recast(){
-    def=test
+function recast_shell(){
+    def=$PWD
     wrk=$(realpath ${1:-$def})
     docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v $wrk:/work -e PACKTIVITY_WORKDIR_LOCATION=/work:$wrk recast/recast-atlas-preptool:latest bash
 }
+
+function recast_run(){
+    wrk=$(realpath ${1})
+    analysis=${2}
+    docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $wrk:/work -e PACKTIVITY_WORKDIR_LOCATION=/work:$wrk recast/recast-atlas-preptool:latest recast run $analysis
+}
+
