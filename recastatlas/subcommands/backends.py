db45dd19585e2e2a75a0b179b06fd19d5d0e5024
Lukas Heinrich <lukas.heinrich@gmail.com>
2019-05-14 14:16:56 +0200

    backends

--- recastatlas/subcommands/backends.py
+++ recastatlas/subcommands/backends.py
@@ -8,9 +8,14 @@ def backends():
     pass
 
 @backends.command()
-def ls():
+@click.option('--check/--no-check', default = False)
+def ls(check):
     fmt = '{0:20}{1:60}{2:10}'
     click.secho(fmt.format('NAME','DESCRIPTION','STATUS'))
     for k,v in config.backends.items():
-        click.secho(fmt.format(k,v['metadata']['short_description'],'OK' if check_backend(k) else 'NOT OK'))
+        if check:
+                status = 'OK' if check_backend(k) else 'NOT OK'
+        else:
+                status = ''
+        click.secho(fmt.format(k,v['metadata']['short_description'],status))
         
