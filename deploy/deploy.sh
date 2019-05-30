d961cf6398623cc64937196048ac0a061738a61c
Lukas Heinrich <lukas.heinrich@gmail.com>
2019-05-30 22:19:09 +0200

    black

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,9 @@
 
 #!/bin/bash
+echo "login to Docker Hub"
 docker login -u "$DOCKERLOGIN" -p "$DOCKERPW"
+echo "show images"
+docker images
+echo "push to Docker Hub"
 docker push recast/recastatlas:$TRAVIS_BRANCH
 
