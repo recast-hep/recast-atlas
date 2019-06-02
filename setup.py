472805ec77c2c14ecef794ba29474152802a89a0
Lukas Heinrich <lukas.heinrich@gmail.com>
2019-06-02 17:52:45 +0200

    Bump version: 0.0.10 → 0.0.11

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def _is_test_pypi():
 
 setup(
   name = 'recast-atlas',
-  version = '0.0.10',
+  version = '0.0.11',
   description = 'RECAST for ATLAS at the LHC',
   url = '',
   author = 'Lukas Heinrich',
