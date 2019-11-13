876796481556524f734ba84cf06dcfa15633c683
Lukas Heinrich <lukas.heinrich@gmail.com>
2019-11-13 02:36:26 +0100

    Bump version: 0.0.18 → 0.0.19

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as readme_md
 
 setup(
   name = 'recast-atlas',
-  version = '0.0.18',
+  version = '0.0.19',
   description = 'RECAST for ATLAS at the LHC',
   url = '',
   author = 'Lukas Heinrich',
