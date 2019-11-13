ff0974dec28ad9ad4422a77e9735d28c7b992bbc
Lukas Heinrich <lukas.heinrich@gmail.com>
2019-11-13 02:08:04 +0100

    move to srcush

--- setup.py
+++ setup.py
@@ -8,28 +8,6 @@ if sys.version_info.major < 3:
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as readme_md:
     long_description = readme_md.read()
 
-def _is_test_pypi():
-    """
-    Determine if the Travis CI environment has TESTPYPI_UPLOAD defined and
-    set to true (c.f. .travis.yml)
-
-    The use_scm_version kwarg accepts a callable for the local_scheme
-    configuration parameter with argument "version". This can be replaced
-    with a lambda as the desired version structure is {next_version}.dev{distance}
-    c.f. https://github.com/pypa/setuptools_scm/#importing-in-setuppy
-
-    As the scm versioning is only desired for TestPyPI, for depolyment to PyPI the version
-    controlled through bumpversion is used.
-    """
-    from os import getenv
-
-    return (
-        {'local_scheme': lambda version: ''}
-        if getenv('TESTPYPI_UPLOAD') == 'true'
-        else False
-    )
-
-
 setup(
   name = 'recast-atlas',
   version = '0.0.17',
@@ -68,5 +46,4 @@ setup(
   },
   dependency_links = [
   ],
-  use_scm_version=_is_test_pypi(),
 )
