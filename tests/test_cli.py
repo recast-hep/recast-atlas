af6b206f25aa32b93fe3fb35c17bc66629a38396
Lukas Heinrich <lukas.heinrich@gmail.com>
2021-07-12 12:36:19 +0200

    tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,7 @@
 from click.testing import CliRunner
 from recastatlas.cli import recastatlas
 from recastatlas.subcommands.run import run
+from recastatlas.subcommands.catalogue import catalogue
 
 def test_cli():
     runner = CliRunner()
@@ -10,4 +11,9 @@ def test_cli():
 def test_run_hello_world():
     runner = CliRunner()
     test = runner.invoke(run,['testing/busyboxtest','--backend','local'])
+    assert test.exit_code == 0
+
+def test_run_catalogue():
+    runner = CliRunner()
+    test = runner.invoke(catalogue,['ls'])
     assert test.exit_code == 0
