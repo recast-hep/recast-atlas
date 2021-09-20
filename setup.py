7dc2dd98729bf2f8559fc477efe511232665fa94
Matthew Feickert <matthew.feickert@cern.ch>
2021-09-20 00:40:42 -0500

    build: Remove all information from setup.py now in setup.cfg

--- setup.py
+++ setup.py
@@ -1,47 +1,18 @@
-from setuptools import setup, find_packages
-from os import path
-import sys
+from setuptools import setup
 
-this_directory = path.abspath(path.dirname(__file__))
-if sys.version_info.major < 3:
-    from io import open
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as readme_md:
-    long_description = readme_md.read()
-
-setup(
-    name='recast-atlas',
-    version='0.1.7',
-    description='RECAST for ATLAS at the LHC',
-    url='',
-    author='Lukas Heinrich',
-    author_email='lukas.heinrich@cern.ch',
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
-    include_package_data=True,
-    install_requires=[
-        'click',
-        'jsonschema',
-        'pyyaml',
+extras_require = {
+    'develop': {'pytest', 'flake8>=3.9.0', 'black'},
+    'local': [
+        'pydotplus==2.0.2',
+        'adage==0.10.1',
         'yadage-schemas==0.10.6',
+        'packtivity==0.14.23',
+        'yadage==0.20.1',  # yadage[viz] breaks so install following manually
+        'pydot',  # from yadage[viz] extra
+        'pygraphviz',  # from yadage[viz] extra
     ],
-    extras_require={
-        'develop': {'pytest', 'flake8>=3.9.0', 'black'},
-        'local': [
-            'pydotplus==2.0.2',
-            'adage==0.10.1',
-            'yadage-schemas==0.10.6',
-            'packtivity==0.14.23',
-            'yadage==0.20.1',  # yadage[viz] breaks so install following manually
-            'pydot',  # from yadage[viz] extra
-            'pygraphviz',  # from yadage[viz] extra
-        ],
-        'kubernetes': ['kubernetes==9.0.0'],
-        'reana': ['reana-client==0.7.5'],
-    },
-    entry_points={
-        'console_scripts': [
-            'recast=recastatlas.cli:recastatlas',
-        ],
-    },
-    dependency_links=[],
-)
+    'kubernetes': ['kubernetes==9.0.0'],
+    'reana': ['reana-client==0.7.5'],
+}
+
+setup(extras_require=extras_require)
