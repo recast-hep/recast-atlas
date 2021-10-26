3069ea029425573b897a3562fdd53d1e6de6835a
Matthew Feickert <matthew.feickert@cern.ch>
2021-10-25 22:29:28 -0500

    feat: Report 'NOT AVAILABLE' if backend unloaded
    If a backend is unloadable during `recast backends ls --check`, instead
    of raising a KeyError, instead catch the KeyError, raise a
    recastatlas.exceptions.BackendNotAvailableException, and report 'NOT
    AVAILABLE' for the STATUS.

--- src/recastatlas/subcommands/backends.py
+++ src/recastatlas/subcommands/backends.py
@@ -1,6 +1,7 @@
 import click
 from ..config import config
 from ..backends import check_backend
+from recastatlas.exceptions import BackendNotAvailableException
 
 
 @click.group(help="The RECAST computational backends.")
@@ -13,12 +14,22 @@ def backends():
 def ls(check):
     fmt = "{0:20}{1:60}{2:10}"
     click.secho(fmt.format("NAME", "DESCRIPTION", "STATUS"))
-    for k, v in config.backends.items():
+    for backend, conf in config.backends.items():
         if check:
-            status = "OK" if check_backend(k) else "NOT OK"
+            try:
+                status = "OK" if check_backend(backend) else "NOT OK"
+            except BackendNotAvailableException:
+                # check_backend is performed by iterating through the global
+                # dict recastatlas.backends.BACKENDS and checking each backend
+                # key. If a backend is missing from the installation it will
+                # not be loaded into the dict and will raise a
+                # recastatlas.exceptions.BackendNotAvailableException.
+                status = "NOT AVAILABLE"
         else:
             status = ""
         default = {"short_description": "no description given"}
         click.secho(
-            fmt.format(k, v.get("metadata", default)["short_description"], status)
+            fmt.format(
+                backend, conf.get("metadata", default)["short_description"], status
+            )
         )
