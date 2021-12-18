846821165c50ef679438447e93f6e69696c33663
Matthew Feickert <matthew.feickert@cern.ch>
2021-12-17 18:43:32 -0600

    Set lower bounds for adagae and yadage-schemas

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ extras_require = {
     'develop': {'pytest', 'flake8>=3.9.0', 'black'},
     'local': [
         'pydotplus==2.0.2',
-        'adage==0.10.1',
-        'yadage-schemas==0.10.6',
+        'adage>=0.10.1',
+        'yadage-schemas>=0.10.6',
         'packtivity>=0.14.23',
         'yadage==0.20.1',  # yadage[viz] breaks so install following manually
         'pydot',  # from yadage[viz] extra
