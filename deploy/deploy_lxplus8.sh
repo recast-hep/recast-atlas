5d35f0ca2c587b4d66e9e9c57636fcebc8b60201
Matthew Feickert <matthew.feickert@cern.ch>
2021-12-17 18:39:19 -0600

    feat: Add LXPLUS8 deploy scripts and requirements.txt (#85)
    * Add a requirements.txt file and .lock file for the version of recast-atlas.
       - New versions should be generated for each deployment and added
         while keeping older ones.
       - The "lockfile" is generated using pip-tools.
    * Add a deploy setup script for deployment on LXPLUS8.
    * Update the README for a working example.
    * Update maintainer notes with deployment instructions and an overview of
    how pip-tools should be used.
    * Remove the old lxplus_reqs.txt as it is outdated.

---
+++ deploy/deploy_lxplus8.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# Example usage:
+# $ bash deploy_lxplus8.sh 0.1.9
+# or to get the latest recast-atlas release by default:
+# $ bash deploy_lxplus8.sh
+
+# Ensure that pip can't install outside a venv
+export PIP_REQUIRE_VIRTUALENV=true
+
+# Ensure local virtualenv setup
+if [ ! -f "${HOME}/opt/venv/bin/virtualenv" ]; then
+    curl -sL --location --output /tmp/virtualenv.pyz https://bootstrap.pypa.io/virtualenv.pyz
+    python3 /tmp/virtualenv.pyz ~/opt/venv
+    ~/opt/venv/bin/pip install --upgrade pip
+    ~/opt/venv/bin/pip install virtualenv
+    mkdir -p ~/bin  # Ensure exists if new machine
+    ln -s ~/opt/venv/bin/virtualenv ~/bin/virtualenv
+fi
+
+RECAST_ATLAS_VERSION="${1:-0.0.0}"
+
+if [[ "${RECAST_ATLAS_VERSION}" == "0.0.0" ]]; then
+    # LXPLUS8 has an ancient version of pip, so need to create a virtual environment
+    # to bootstrap into a useable version and use virtualenv as it is significantly
+    # faster
+    virtualenv --quiet __venv__
+    . __venv__/bin/activate
+    python -m pip --quiet install --upgrade pip
+
+    # `pip index versions` will output the following warning to stderr:
+    # WARNING: pip index is currently an experimental command. It may be removed/changed in a future release without prior warning.
+    # so direct stderr to /dev/null to avoid this.
+    # The next line will be:
+    # recast-atlas (x.y.z)
+    # so filter out the version number and output that
+    RECAST_ATLAS_VERSION=$(python -m pip index versions recast-atlas 2> /dev/null | awk -F"[()]" 'NR==1 {print $2}')
+
+    # cleanup
+    deactivate
+    rm -r __venv__
+fi
+
+# Setup Python virtual environment and install recast-atlas and requirements
+python3 -m venv ~recast/public/recast-atlas-"${RECAST_ATLAS_VERSION}"
+. ~recast/public/recast-atlas-"${RECAST_ATLAS_VERSION}"/bin/activate
+
+if [[ "${RECAST_ATLAS_VERSION}" == "0.1.8" ]]; then
+    # c.f. https://github.com/reanahub/reana-client/issues/558
+    python -m pip install --upgrade pip 'setuptools<58.0.0' wheel
+else
+    python -m pip install --upgrade pip setuptools wheel
+fi
+
+# Install the environment from a "lockfile" (just called a requirements.txt by
+# pip-tools) generated with pip-tools compile. If the lockfile is missing, make
+# compile one with pip-tools.
+if [ ! -f ~recast/deploy/recast-atlas-"${RECAST_ATLAS_VERSION}".lock ]; then
+    python -m pip install pip-tools
+    python -m piptools compile \
+        --generate-hashes \
+        --output-file ~recast/deploy/recast-atlas-"${RECAST_ATLAS_VERSION}".lock \
+        ~recast/deploy/recast-atlas-"${RECAST_ATLAS_VERSION}"-requirements.txt
+fi
+python -m pip install --requirement ~recast/deploy/recast-atlas-"${RECAST_ATLAS_VERSION}".lock
+
+# Use heredoc syntax to generate public environment setup script
+cat << EOF > ~recast/public/setup_"${RECAST_ATLAS_VERSION}".sh
+#!/bin/bash
+
+export RECAST_DEFAULT_RUN_BACKEND=local
+export RECAST_DEFAULT_BUILD_BACKEND=kubernetes
+export PACKTIVITY_CONTAINER_RUNTIME=singularity
+export SINGULARITY_CACHEDIR="/tmp/\$(whoami)/singularity"
+mkdir -p "\${SINGULARITY_CACHEDIR}"
+
+# https://twitter.com/lukasheinrich_/status/1021398718996713475
+export LC_ALL=en_US.utf-8
+export LANG=en_US.utf-8
+
+. ~recast/public/recast-atlas-${RECAST_ATLAS_VERSION}/bin/activate
+
+\$(recast catalogue add /eos/project/r/recast/atlas/catalogue)
+export KUBECONFIG=/eos/project/r/recast/atlas/cluster/clusterconfig
+export PATH="\${PATH}:~recast/public/bin"
+EOF
+
+# Link public setup script to this version
+ln --symbolic --force ~recast/public/setup_"${RECAST_ATLAS_VERSION}".sh ~recast/public/setup.sh
--- docs/maintainer-notes.md
+++ docs/maintainer-notes.md
@@ -71,3 +71,27 @@ to perform local tests.
 
 5. Enter any release notes and click "Publish release".
    * This then kicks of the publication CD workflow that will use the PyPI API key to publish.
+
+## Deployment to [LXPLUS8](https://clouddocs.web.cern.ch/clients/lxplus.html)
+
+When a new release of `recast-atlas` is out the maintainers should open a PR to produce a new deployment `requirements.txt` at `deploy/recast-atlas-x.y.z-requirements.txt`.
+This should be done by unpinning everything, installing the requirements into a Python virtual environment, and then using `python -m pip freeze` to determine the versions that things should be pinned at for deployment.
+This only will specify the core requirements.
+
+To properly specify a full deployment environment a lock file is created with [`pip-tools`](https://github.com/jazzband/pip-tools/).
+Once the `deploy/recast-atlas-x.y.z-requirements.txt` exists, use `pip-tools` to compile a deployment environment lock file which defines the environment down to the hash level
+
+```console
+python -m piptools compile \
+    --generate-hashes \
+    --output-file deploy/recast-atlas-x.y.z.lock \
+    deploy/recast-atlas-x.y.z-requirements.txt
+```
+
+The resulting `requirements.txt` file and `.lock` file should be uploaded to LXPLUS8 under `~recast/deploy/`.
+
+Following that, the deployment script on LXPLUS (located under `~recast/deploy/`) should get rerun with the new version tag (`x.y.z`) as input
+
+```
+bash deploy_lxplus8.sh x.y.z
+```
