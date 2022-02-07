3f3751d21eddf75222e5a3eaad9d5784bce3b804
Matthew Feickert <matthew.feickert@cern.ch>
2022-02-07 10:49:04 -0600

    Simplify local requirements

--- setup.py
+++ setup.py
@@ -3,12 +3,8 @@ from setuptools import setup
 extras_require = {
     'develop': {'pytest', 'flake8>=3.9.0', 'black'},
     'local': [
-        'pydotplus==2.0.2',
-        'adage>=0.10.1',
         'packtivity>=0.14.23',
-        'yadage>=0.20.1',  # yadage[viz] breaks so install following manually
-        'pydot',  # from yadage[viz] extra
-        'pygraphviz',  # from yadage[viz] extra
+        'yadage[viz]>=0.20.2'
     ],
     'kubernetes': ['kubernetes==9.0.0'],
     'reana': ['reana-client>=0.8.0'],
