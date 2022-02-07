d4d75a4a4ca994bcf125b81e252c1208a4e296be
Matthew Feickert <matthew.feickert@cern.ch>
2022-02-07 10:49:21 -0600

    Apply black

--- setup.py
+++ setup.py
@@ -2,10 +2,7 @@ from setuptools import setup
 
 extras_require = {
     'develop': {'pytest', 'flake8>=3.9.0', 'black'},
-    'local': [
-        'packtivity>=0.14.23',
-        'yadage[viz]>=0.20.2'
-    ],
+    'local': ['packtivity>=0.14.23', 'yadage[viz]>=0.20.2'],
     'kubernetes': ['kubernetes==9.0.0'],
     'reana': ['reana-client>=0.8.0'],
 }
